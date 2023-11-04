0eb99a0e7fd2aa6ba2cb64e965edb9b6f4bea737
giugiu <carla.giuliani.2@etu.sorbonne-universite.fr>
2023-11-04 11:58:26 +0100

    je push pour pull

--- backend/GetInfos.tsx
+++ backend/GetInfos.tsx
@@ -1,42 +1,42 @@
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'
 
-import {mainAddress, mainABI} from "./constants.js"
+import { mainAddress, mainABI } from './constants.js'
 
-export async function getCardUser() : Promise<string[]>{
-  return new Promise((resolve) => {  
+export async function getCardUser(): Promise<string[]> {
+  return new Promise(resolve => {
     if (window.ethereum) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const contract = new ethers.Contract(mainAddress, mainABI, provider);
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(mainAddress, mainABI, provider)
 
-        provider.getSigner().getAddress().then(async (address) => {
-          const transaction = await contract.createCollection(address);
-          await transaction.wait();
-          resolve(transaction);
-        });
-    
-        
+      provider
+        .getSigner()
+        .getAddress()
+        .then(async address => {
+          const transaction = await contract.createCollection(address)
+          await transaction.wait()
+          resolve(transaction)
+        })
     }
   })
 }
 
 export async function getSetsOwner(nameSet) {
-  return new Promise(async (resolve) => {  
+  return new Promise(async resolve => {
     if (window.ethereum) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const contract = new ethers.Contract(mainAddress, mainABI, provider)
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(mainAddress, mainABI, provider)
 
-        const cardsId = await contract.getCardIdCollection(nameSet);
-        await cardsId.wait();
+      const cardsId = await contract.getCardIdCollection(nameSet)
+      await cardsId.wait()
 
-        var infos= {}
-        cardsId.forEach(async cardId => {
-          const owners = await contract.getUsersCardCollection(nameSet, cardId);
-          await owners.wait();
-          infos[cardId] = owners; 
-        });
-    
-        resolve(infos);
-        
+      var infos = {}
+      cardsId.forEach(async cardId => {
+        const owners = await contract.getUsersCardCollection(nameSet, cardId)
+        await owners.wait()
+        infos[cardId] = owners
+      })
+
+      resolve(infos)
     }
   })
 }
