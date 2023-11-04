98c79e54fd6ab177cb07ab99ffeaf292833e1902
giugiu <carla.giuliani.2@etu.sorbonne-universite.fr>
2023-11-04 18:08:15 +0100

    erreur import

--- frontend/src/backend/Booster.tsx
+++ frontend/src/backend/Booster.tsx
@@ -1,36 +1,36 @@
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'
 
-import { useWallet } from '@/App.js';
+import { useWallet } from '../App.tsx'
 
-export async function buyBooster(name: string) : Promise<boolean> {
-    return new Promise(async (resolve) => {
-			try {
-				const wallet = useWallet();
-				if (wallet) {
-					const provider = wallet.details.provider;
-					const account = wallet.details.account;
-					const contract = wallet.contract;
-					if(account && contract){
-						provider.getBalance(account).then(async (balance) => {
-							const balanceInEther = ethers.utils.formatEther(balance);
-							const cost = await contract.getCostBooster(name);
-							await cost.wait();
+export async function buyBooster(name: string): Promise<boolean> {
+  return new Promise(async resolve => {
+    try {
+      const wallet = useWallet()
+      if (wallet) {
+        const provider = wallet.details.provider
+        const account = wallet.details.account
+        const contract = wallet.contract
+        if (account && contract) {
+          provider
+            .getBalance(account)
+            .then(async balance => {
+              const balanceInEther = ethers.utils.formatEther(balance)
+              const cost = await contract.getCostBooster(name)
+              await cost.wait()
 
-							if (balanceInEther >= cost) {
-									const transaction = await contract.buyBooster(account, name);
-									await transaction.wait();
-									resolve(true);
-									
-							}
-								
-						}).catch((error) => {
-								console.error('Erreur lors de la récupération du solde :', error);
-						});
-					}
-							
-				}
-			} catch (error) {
-					console.error("Erreur lors de l'achat du booster :", error);
-			}
-		})
+              if (balanceInEther >= cost) {
+                const transaction = await contract.buyBooster(account, name)
+                await transaction.wait()
+                resolve(true)
+              }
+            })
+            .catch(error => {
+              console.error('Erreur lors de la récupération du solde :', error)
+            })
+        }
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'achat du booster :", error)
+    }
+  })
 }
